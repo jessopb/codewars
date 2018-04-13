7029298a5b048d973b835db941f4b58ef3e94cc8
Jessop Breth <jessopb@gmail.com>
2018-04-12 21:27:12 -0400

    tests passed

--- python/sum_of_two_lowest-7kyu/sum_smallest.py
+++ python/sum_of_two_lowest-7kyu/sum_smallest.py
@@ -1,9 +1,8 @@
 def sum_two_smallest_numbers(numbers):
 
     """ return sum of the smallest two integers in a list
-
     >>> sum_two_smallest_numbers([37,51,95,23])
-    50
+    60
     >>> sum_two_smallest_numbers([5, 8, 12, 18, 22])
     13
     >>> sum_two_smallest_numbers([7, 15, 12, 18, 22])
@@ -11,34 +10,28 @@ def sum_two_smallest_numbers(numbers):
     >>> sum_two_smallest_numbers([25, 42, 12, 18, 22])
     30
 
-    >>> sum_two_smallest_numbers()
-    Traceback (most recent call last):
-     ...
-    TypeError: (input must be integer list with length at least 4)
     >>> sum_two_smallest_numbers([5, 8, 12, .18, 22])
     Traceback (most recent call last):
      ...
-    TypeError: (input must be integer list with length at least 4)
+    TypeError: all list members must be integers
     >>> sum_two_smallest_numbers([5, 8, 12])
     Traceback (most recent call last):
      ...
-    TypeError: (input must be integer list with length at least 4)
+    ValueError: input list must have length at least 4
     >>> sum_two_smallest_numbers('hello')
     Traceback (most recent call last):
      ...
-    TypeError: (input must be integer list with length at least 4)
+    TypeError: input must be integer list
     """
-    if not isinstance(s, list):
-        raise TypeError('input must be integer list with length at least 4')
-    if len(s) < 4:
-        raise TypeError('input must be integer list with length at least 4')
-    for x in range(len(s)):
-        if not isinstance(s[x], int):
-            raise TypeError('input must be integer list with length at least 4')
+
+    if not isinstance(numbers, list):
+        raise TypeError('input must be integer list')
+    if len(numbers) < 4:
+        raise ValueError('input list must have length at least 4')
+    for x in range(len(numbers)):
+        if not isinstance(numbers[x], int):
+            raise TypeError('all list members must be integers')
         else:
             pass
-    else:
-        pass
-
-
-## raise TypeError('most recent call last')
+    numbers.sort()
+    return numbers[0] + numbers[1]
