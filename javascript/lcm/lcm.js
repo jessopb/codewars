9cf48e0e7fe25ecc097af71c94c1896e8e035992
Jessop Breth <jessopb@gmail.com>
2018-04-13 23:24:17 -0400

    passes all tests

--- javascript/lcm/lcm.js
+++ javascript/lcm/lcm.js
@@ -1,26 +1,15 @@
 function lcm(int1, int2) {
-  mult = int1 * int2
-  high = int1 >= int2 ? int1 : int2
+  let m = []
+  let b = int1 > int2 ? int1 : int2
+  let s = int1 < int2 ? int1 : int2
 
-  let findlcm = (i, j, m, h) => {
-    if (m > h) {
-      for (let n=2; n <= Math.ceil(Math.sqrt(m)); n++)
-      {
-        if (m % n == 0)
-        {
-          if ((i % n == 0) && (j % n == 0))
-          {
-            return findlcm(i, j, m / n, h)
-          }
-          else return m
-        }
-      } //for
-    }
-    else
-    {
-      return m
-    }
-  }//findlcm
-  return findlcm(int1, int2, mult, high)
+  for (let x = b; x <= b * s; x += b)
+  {
+    m.push(x)
+  }
+  for (let i = s; i <= b*s; i += s)
+  {
+    if (m.includes(i)) return i
+  }
 }
 module.exports = lcm
