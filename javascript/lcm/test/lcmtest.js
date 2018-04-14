855a0de34651f2baa14b0cfa87e2b119b071a872
Jessop Breth <jessopb@gmail.com>
2018-04-13 22:41:14 -0400

    new tests

--- javascript/lcm/test/lcmtest.js
+++ javascript/lcm/test/lcmtest.js
@@ -10,4 +10,9 @@ describe('should return least common multiple of two integers', function () {
     expect(lcm(4,8)).to.equal(8)
     expect(lcm(3,27)).to.equal(27)
   })
+  it('should return lcm when it is neither one of the integers nor the product', function() {
+    expect(lcm(6,10)).to.equal(30)
+    expect(lcm(4,10)).to.equal(20)
+    expect(lcm(4,18)).to.equal(36)
+  })
 })
